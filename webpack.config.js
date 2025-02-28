42252fb9b1bbc2e4afee0ca6d90b477118a25fd5
IgYel <cutelyfoxy@gmail.com>
2025-02-28 08:16:40 +0100

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,66 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "./",
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/",
+    },
+    mode: isProduction ? "production" : "development",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },
-      {
-        test: /\.scss$/, // Поддержка SCSS
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.css$/, // Добавляем поддержку CSS
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  devServer: {
-    static: path.resolve(__dirname, "public"), // Путь к статике
-    hot: true,
-    port: 8080,
-    open: true, // Автоматически открывать в браузере
-  },
-  plugins: [
-    new CleanWebpackPlugin(), // Очищает папку dist перед сборкой
-    new HtmlWebpackPlugin({
-      template: "./public/index.html", // Указывает шаблон для HTML
-      filename: "index.html",
-    }),
-  ],
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|webp)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/images/[name][ext]",
+          },
+        },
+        {
+          test: /\.(woff2?|eot|ttf|otf)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/fonts/[name][ext]",
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    devServer: {
+      static: path.resolve(__dirname, "public"),
+      hot: true,
+      port: 8080,
+      open: true,
+      historyApiFallback: true,
+    },
+    plugins: [
+      isProduction ? new CleanWebpackPlugin() : null,
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "index.html",
+      }),
+    ].filter(Boolean),
+  };
 };
